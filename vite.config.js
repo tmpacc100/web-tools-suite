e7b01626542685df8f2776fda0edea6473bf299c
tmpacc100 <trace9517@gmail.com>
2026-01-13 22:47:19 +0900

    Move ads.txt to root and update base path

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,5 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: './',
-    build: {
-        outDir: 'dist',
-        emptyOutDir: true
-    }
-});
+    base: '/web-tools-suite/',
+})
